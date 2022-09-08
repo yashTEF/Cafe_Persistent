fbcc04b575d7f11b2e4b1544cf84327b23d81e75
yashTEF <yashsoc@gmail.com>
2022-09-08 22:09:06 +0530

    Readme Added

---
+++ src/main/java/com/cafe_persistent/App.java
@@ -0,0 +1,13 @@
+package com.cafe_persistent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
